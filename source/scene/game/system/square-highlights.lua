a6565f5b43ed85cca08ce360d0dbd96ff97e1083
Andrew Minnich <aminnich3@gmail.com>
2020-02-11 03:06:27 -0500

    fix square highlights not activating on the first frame

--- source/scene/game/system/square-highlights.lua
+++ source/scene/game/system/square-highlights.lua
@@ -3,8 +3,7 @@ local SquareHighlight = require 'scene.game.entity.square-highlight'
 
 local squareHighlights = {}
 
-function squareHighlights:addToGroup(groupName, board)
-	if groupName ~= 'board' then return end
+function squareHighlights:init(groupName, board)
 	for x = 0, constant.boardWidth - 2 do
 		for y = 0, constant.boardHeight - 2 do
 			self.pool:queue(SquareHighlight(self.pool, x, y))
