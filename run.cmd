f4746b6b8c503e37563a8675673c1ed89104bba3
Andrew Minnich <aminnich3@gmail.com>
2020-02-16 17:59:39 -0500

    run script

---
+++ run.cmd
@@ -0,0 +1,25 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: export tiled maps
+echo;
+echo exporting tiled maps
+echo --------------------
+for %%f in (source/puzzle/*.tmx) do (
+	set tmxPath=source/puzzle/%%f
+	set luaPath=!tmxPath:~0,-4!.lua
+	echo exporting !tmxPath! to !luaPath!
+	tiled --export-map !tmxPath! !luaPath!
+)
+
+:: run love
+echo;
+echo running love
+echo ------------
+lovec source
+
+:: remove ignored files
+echo;
+echo cleaning up files
+echo -----------------
+git clean -Xdf
