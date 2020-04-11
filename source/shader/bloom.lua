024e222f87582327835f29640a9fa0d72c432a30
Andrew Minnich <aminnich3@gmail.com>
2020-04-11 02:27:21 -0400

    multiple downscaled canvases (still looks pretty crappy)

--- source/shader/bloom.lua
+++ source/shader/bloom.lua
@@ -1,48 +1,55 @@
-local constant = require 'constant'
 local Object = require 'lib.classic'
 
 local Bloom = Object:extend()
 
-Bloom.blurCanvasDownsampleFactor = 20
+Bloom.stages = 10
+
+function Bloom:createCanvases()
+	self.canvases = {}
+	for i = 1, self.stages do
+		local scale = 1 / (2 ^ (i - 1))
+		table.insert(self.canvases, {
+			love.graphics.newCanvas(
+				love.graphics.getWidth() * scale,
+				love.graphics.getHeight() * scale
+			),
+			stencil = true,
+		})
+	end
+end
 
 function Bloom:new()
-	self.mainCanvas = love.graphics.newCanvas()
-	self.mainCanvas:setFilter('linear', 'linear', 16)
-	self.blurCanvas = love.graphics.newCanvas(
-		constant.screenWidth / self.blurCanvasDownsampleFactor,
-		constant.screenHeight / self.blurCanvasDownsampleFactor
-	)
-	self.blurCanvas:setFilter('linear', 'linear', 16)
-	self.mainCanvasOptions = {self.mainCanvas, stencil = true}
-	self.blurCanvasOptions = {self.blurCanvas, stencil = true}
+	self:createCanvases()
 end
 
 function Bloom:resize()
-	self.mainCanvas = love.graphics.newCanvas()
-	self.mainCanvas:setFilter('linear', 'linear', 16)
-	self.mainCanvasOptions = {self.mainCanvas, stencil = true}
+	self:createCanvases()
 end
 
 function Bloom:start()
 	love.graphics.push 'all'
-	love.graphics.setCanvas(self.mainCanvasOptions)
+	love.graphics.setCanvas(self.canvases[1])
 	love.graphics.clear()
 end
 
 function Bloom:finish()
 	love.graphics.pop()
-	love.graphics.push 'all'
-	love.graphics.setCanvas(self.blurCanvasOptions)
-	love.graphics.clear()
-	love.graphics.draw(self.mainCanvas, 0, 0, 0, 1 / self.blurCanvasDownsampleFactor)
-	love.graphics.pop()
 
-	love.graphics.push 'all'
-	love.graphics.draw(self.mainCanvas)
-	love.graphics.setColor(1, 1, 1, 1/4)
-	love.graphics.setBlendMode 'add'
-	love.graphics.draw(self.blurCanvas, 0, 0, 0, self.blurCanvasDownsampleFactor)
-	love.graphics.pop()
+	love.graphics.draw(self.canvases[1][1])
+
+	for i = 2, self.stages do
+		love.graphics.push 'all'
+		love.graphics.setCanvas(self.canvases[i])
+		love.graphics.clear()
+		love.graphics.draw(self.canvases[i - 1][1], 0, 0, 0, .5)
+		love.graphics.pop()
+
+		love.graphics.push 'all'
+		love.graphics.setBlendMode 'add'
+		love.graphics.setColor(1, 1, 1, 1/8)
+		love.graphics.draw(self.canvases[i][1], 0, 0, 0, 2 ^ (i - 1))
+		love.graphics.pop()
+	end
 end
 
 return Bloom
